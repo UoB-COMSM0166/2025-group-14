403f43b4c51b4058f065bb79027aabaa775971df
Polly Lang <ukpollylang@gmail.com>
2025-02-06 11:42:54 +0000

    Minor modifications to Pursuer methods. Commented the code for readability.

--- Pursuit_feature_v1/Player.js
+++ Pursuit_feature_v1/Player.js
@@ -9,6 +9,12 @@ class Player {
         this.upperYBound = canal.bottom - (this.w/2);
     }
 
+    // Player movement - pressing the up, down, left, right arrow keys causes the
+    // player's velocity vector to change to a vector with 0 magnitude in the opposite dimension
+    // and a magnitude of this.speed in the indicated direction. E.g. if the player 
+    // presses the down key, the velocity changes to (0, this.speed) i.e. a magnitude of 
+    // this.speed in the y-dimension. Upward movement is the same vector with opposite direction,
+    // i.e. (0, -this.speed).
     move() {
         if (keyIsPressed === true) {
             if (keyCode === 40 && this.position.y < this.upperYBound) {
@@ -27,13 +33,14 @@ class Player {
             
           }
     }
-  
+
+    // Draws the player to the canvas
     show() {
       noStroke();
       fill(255, 100);
       push();
-      translate(this.position.x, this.position.y);
-      rotate(this.velocity.heading());
+      translate(this.position.x, this.position.y); // translates the player to its position vector
+      rotate(this.velocity.heading()); // rotates the direction the player is facing to the vector heading
       ellipse(0, 0, this.w, this.h);
       pop();
     }
