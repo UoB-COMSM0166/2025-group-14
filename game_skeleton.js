93993d59e54a410c8e2e89d4edeaea409a8e38c7
Casey Cronyn <casey.cronyn@gmail.com>
2025-02-06 15:02:50 +0000

    remove hard coded boundaries from boat

--- game_skeleton.js
+++ game_skeleton.js
@@ -1,6 +1,3 @@
-let oldWindowWidth;
-let oldWindowHeight;
-
 let x1;
 let y1;
 let w = 80;
@@ -12,48 +9,40 @@ let prevKey = "horisontal";
 function setup() {
   //set the canvas size the first time when the program starts
   createCanvas(windowWidth, windowHeight);
-  oldWindowWidth = windowWidth;
-  oldWindowHeight = windowHeight;
 
   x1 = width / 2;
   y1 = height / 2;
 }
 
 function draw() {
-
   background(200);
   // Update x and y if a key is pressed.
   if (keyIsPressed === true) {
-    if (key === 'w' && y1 > height/3 + 40) {
+    if (key === 'ArrowUp') {
       chngDirShape();
       y1 -= 2;
-    } else if (key === 's' && y1 < height*2/3 - 40) {
+    } else if (key === 'ArrowDown') {
       chngDirShape();
       y1 += 2;
-    } else if (key === 'a') {
+    } else if (key === 'ArrowLeft') {
       chngDirShape();
       x1 -= 2;
-    } else if (key === 'd') {
+    } else if (key === 'ArrowRight') {
       chngDirShape();
       x1 += 2;
     }
   }
-mod
   fill(0);
 
   ellipse(x1, y1, w, h);
-
-  strokeWeight(3);
-  line(0, height/3, width, height/3);
-  line(0, height*2/3, width, height*2/3);
 }
 
 function chngDirShape() {
-  if (prevKey === "horisontal" && (key === 'w' || key === 's')){
+  if (prevKey === "horisontal" && (key === 'ArrowUp' || key === 'ArrowDown')){
     chngShape()
     prevKey = "vertical";
   }
-  else if (prevKey === "vertical" && (key === 'a' || key === 'd')){
+  else if (prevKey === "vertical" && (key === 'ArrowLeft' || key === 'ArrowRight')){
     chngShape()
     prevKey = "horisontal";
   }
