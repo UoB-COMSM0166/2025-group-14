a279088f68e2a8069e49a1ce6a8acdffefc09713
adam-sid <148649495+adam-sid@users.noreply.github.com>
2025-02-08 13:57:10 +0000

    change velocity logic - allows diagonal movement

--- adam_prototyping/sketch.js
+++ adam_prototyping/sketch.js
@@ -1,42 +1,51 @@
-let pixels = 100;
-let width = 16 * pixels;
-let height = 9 * pixels;
 let total = 100; // Define the total number of points for the canal
+let aspectRatio = 16/9; 
 
 function setup() {
+  let width = windowWidth;
+  let height = windowWidth / aspectRatio;
   createCanvas(width, height);
-  x1 = width / 2;
-  y1 = height / 2;
+  player = new Player(width/2, height/2);
 }
 
 function draw() {
   background(60, 75, 20);
   new Canal(400, 1.75); // Create an instance of the Canal class
-  
-   
-  if (keyIsDown(UP_ARROW)) {
-    y1 -= 2;
-    if (keyIsDown(LEFT_ARROW)) {
-     x1 -= 2;
-    } else if (keyIsDown(RIGHT_ARROW)) {
-     x1 += 2;
-    }
-  } else if (keyIsDown(DOWN_ARROW)) {
-    y1 += 2;
-    if (keyIsDown(LEFT_ARROW)) {
-     x1 -= 2;
-    } else if (keyIsDown(RIGHT_ARROW)) {
-     x1 += 2;
-    }
-  } else if (keyIsDown(LEFT_ARROW)) {
-    x1 -= 2;
-  } else if (keyIsDown(RIGHT_ARROW)) {
-    x1 += 2;
-    
+  player.move();
+  player.show();
+}
+
+class Player{
+  constructor(x, y/*, canal*/) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.speed = 3;
+    this.w = width/20;
+    this.h = height/20;
+    //this.lowerYBound = canal.top + (this.w/2);
+    //this.upperYBound = canal.bottom - (this.w/2);
   }
-  noStroke();
-  fill(0);
-  ellipse(x1, y1, pixels/2, pixels/3);
+
+  move() {
+    this.velocity.set(0, 0);
+
+    if (keyIsDown(UP_ARROW)) this.velocity.y = -this.speed;
+    if (keyIsDown(DOWN_ARROW)) this.velocity.y = this.speed;
+    if (keyIsDown(LEFT_ARROW)) this.velocity.x = -this.speed;
+    if (keyIsDown(RIGHT_ARROW)) this.velocity.x = this.speed;
+
+    this.position.add(this.velocity);
+  }
+  
+  show() {
+    noStroke();
+    fill(0);
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(this.velocity.heading());
+    ellipse(0, 0, width/20, height/20);
+    pop();
+  }
 }
 
 class Canal {
