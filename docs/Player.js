fd6670e1b4532763007f363c6f4f5653e78679ed
Daniil Lutskiy <ya24711@bristol.ac.uk>
2025-02-20 15:18:27 +0000

    experimenting with bouncing off the edge

--- docs/Player.js
+++ docs/Player.js
@@ -12,14 +12,16 @@ class Player {
   constructor(mainX, mainY, mainMass, velLimit, canal) {
     this.position = createVector(mainX, mainY);
     this.acceleration = createVector(0, 0);
-    this.w = 20;
-    this.h = 10;
+    this.w = 50;
+    this.h = 20;
     this.velocity = createVector(0, 0);
     this.mass = mainMass;
     this.angle = 0;
     this.mu = 0.02;
     this.velocityLimit = velLimit;
     this.canal = canal;
+    this.hitAny = false;
+    this.collisionOffset = createVector(0, 0);
   }
 
   //this is essentially the main function of the class, which was created to encapsulate the class from draw in main.js
@@ -40,6 +42,13 @@ class Player {
     //tests if the boat has moved to another canal segment, and shifts it there if so
     this.reachedTheNextOne(this.canal);
 
+    let hitUp = this.didHitBorder(0); // 0 - upper border
+    let hitDown = this.didHitBorder(1); // 1 - lower border 
+    let hitLeft = this.didHitBorder(2); // 2 - left border
+    let hitRight = this.didHitBorder(3); // 3 - right border
+    this.hitAny = (hitUp || hitDown || hitRight || hitLeft);
+
+    // if (!this.hitAny){
     if (keyIsDown(DOWN_ARROW) === true) {
       this.applyForce(createVector(0, 0.5));
     }
@@ -52,33 +61,135 @@ class Player {
     else if (keyIsDown(RIGHT_ARROW) === true) {
       this.applyForce(createVector(0.5, 0));
     }
+    // }
 
-    let hitUp = this.didHitBorder(0); // 0 - upper border
-    let hitDown = this.didHitBorder(1); // 1 - lower border 
-    let hitLeft = this.didHitBorder(2); // 2 - left border
-    let hitRight = this.didHitBorder(3); // 3 - right border
+    if (this.hitAny) { 
+      this.acceleration = createVector(0, 0);
 
-    if (hitUp || hitDown || hitRight || hitLeft) {
+      // //collision mechanism for the upper border
+      // if (hitUp) {
+      //   this.position.y += 20;
+      // }
+      // //collision mechanism for the bottom border
+      // if (hitDown) {
+      //   this.position.y -= 20;
+      // }
+      // //collision mechanism for the right border
+      // if (hitRight) {
+      //   this.position.x -= 20;
+      // }
+      // //collision mechanism for the left border
+      // if (hitLeft) {
+      //   this.position.x += 20;
+      // }
 
-      //collision mechanism for the upper border
+      // //collision mechanism for the upper border
+      // let speed = this.velocity.mag();
+      // if (hitUp) {
+      //   if (this.velocity.y < 1) {
+      //     this.velocity = createVector(0, 0.7);
+      //   } else {
+      //     this.velocity.mult(0.7, -0.7);
+      //   }
+      // }
+      // //collision mechanism for the bottom border
+      // if (hitDown) {
+      //   if (speed < 1) {
+      //     this.velocity = createVector(0, -0.7);
+      //   } else {
+      //     this.velocity.mult(0.7, -0.7);
+      //   }
+      // }
+      // //collision mechanism for the right border
+      // if (hitRight) {
+      //   if (speed < 1) {
+      //     this.velocity = createVector(-0.7, 0);
+      //   } else {
+      //     this.velocity.mult(-0.7, 0.7);
+      //   }
+      // }
+      // //collision mechanism for the left border
+      // if (hitLeft) {
+      //   if (speed < 1) {
+      //     this.velocity = createVector(1, 0);
+      //   } else {
+      //     this.velocity.mult(-0.7, 0.7);
+      //   }
+      // }
+
+            //collision mechanism for the upper border
+      let speed = this.velocity.mag();
       if (hitUp) {
-        this.position.y += 20;
+        if (speed < 1) {
+          this.collisionOffset = createVector(0, 2);
+          // this.velocity = createVector(0, 0.7);
+        } else {
+          this.velocity.mult(0.7, 0.7);
+          this.collisionOffset = p5.Vector.mult(this.velocity, createVector(1, -1));
+        }
       }
       //collision mechanism for the bottom border
       if (hitDown) {
-        this.position.y -= 20;
+        if (speed < 1) {
+          this.collisionOffset = createVector(0, -2);
+        } else {
+          this.velocity.mult(0.7, 0.7);
+          this.collisionOffset = p5.Vector.mult(this.velocity, createVector(1, -1));
+        }
       }
       //collision mechanism for the right border
       if (hitRight) {
-        this.position.x -= 20;
+        if (speed < 1) {
+          this.collisionOffset = createVector(-2, 0);
+        } else {
+          this.velocity.mult(0.7, 0.7);
+          this.collisionOffset = p5.Vector.mult(this.velocity, createVector(-1, 1));
+        }
       }
       //collision mechanism for the left border
       if (hitLeft) {
-        this.position.x += 20;
+        if (speed < 1) {
+          this.collisionOffset = createVector(2, 0);
+        } else {
+          this.velocity.mult(0.7, 0.7);
+          this.collisionOffset = p5.Vector.mult(this.velocity, createVector(-1, 1));
+        }
+       
       }
+    }
 
-      this.velocity = createVector(0, 0);
-      this.acceleration = createVector(0, 0);
+    this.applyCollisionOffset();
+
+    fill('black');
+    stroke('white');
+    strokeWeight(2);
+    text(`hitUp: ${hitUp}`, this.position.x - 40, this.position.y - 110);
+    text(`hitDown: ${hitDown}`, this.position.x - 40, this.position.y - 95);
+    text(`this.velocity.y: ${this.velocity.y}`, this.position.x - 40, this.position.y - 80);
+    text(`this.velocity.x: ${this.velocity.x}`, this.position.x - 40, this.position.y - 65);
+    text(`acc: ${this.acceleration}`, this.position.x - 40, this.position.y - 50);
+    // text(`vel: ${this.velocity}`, this.position.x - 40, this.position.y - 35); 
+    text(`this.velocity.mag(): ${this.velocity.mag()}`, this.position.x - 40, this.position.y - 20);
+  }
+
+  applyCollisionOffset() {
+    //this.collision offset
+    this.position.add(this.collisionOffset);
+
+    if (this.collisionOffset.x >  0.1) {
+      this.collisionOffset.x -= 0.1;
+    } else if (this.collisionOffset.x < -0.1) {
+      this.collisionOffset.x += 0.1;
+    } else if (Math.abs(this.collisionOffset.x) < 0.1) {
+      this.collisionOffset.x = 0;
+    }
+
+    if (this.collisionOffset.y >  0.1) {
+      this.collisionOffset.y -= 0.1;
+    } else if (this.collisionOffset.y < -0.1) {
+      this.collisionOffset.y += 0.1;
+    } else if (Math.abs(this.collisionOffset.y) < 0.1) {
+      this.collisionOffset.y = 0;
     }
   }
 
@@ -146,11 +257,12 @@ class Player {
   //the formula for friction is F = -v (reverced copy of the velocity vector) * Mu (arbitrary constant) * N (for our purpose can be equated to object's mass) 
   // change the mu parameter if you want to increase/decrease friction
   friction() {
-    if (this.velocity.mag() > 0.02){ //this is a bugfix (without it, the stationary object would flip 60 times a second)
+    if (this.velocity.mag() > 0.02 && (!this.hitAny)){ //this is a bugfix (without it, the stationary object would flip 60 times a second)
       let friction = this.velocity.copy().normalize().mult(-1);
       // Magnitude of Friction
       friction.setMag(this.mu * this.mass);
       this.applyForce(friction);
+      // text(`friction: ${friction.mag()}`, this.position.x - 40, this.position.y - 35); 
     }
   }
 
@@ -184,11 +296,11 @@ class Player {
     fill('red');
     circle(this.position.x + ((this.h/2) * sin(this.velocity.heading())), this.position.y + ((this.h/2) * -1 * cos(this.velocity.heading())), 5);
 
-    // fill(0);
-    // stroke('white');
-    // strokeWeight(5);
-    // // line(-Infinity, this.position.y, Infinity, this.position.y);
-    // // line(0, this.position.y, windowWidth, this.position.y);
+    fill(0);
+    stroke('white');
+    strokeWeight(5);
+    // line(-Infinity, this.position.y, Infinity, this.position.y);
+    // line(0, this.position.y, windowWidth, this.position.y);
 
     // // upper
     // line(0, this.canal.getUpperLimit(this.position.x), windowWidth, this.canal.getUpperLimit(this.position.x));
@@ -218,7 +330,7 @@ class Player {
     // text(`lower: ${Math.round(this.canal.getLowerLimit(this.position.x))}`, this.position.x - 40, this.position.y - 65);
     // text(`x: ${Math.floor(this.position.x)} y: ${Math.floor(this.position.y)}`, this.position.x - 40, this.position.y - 50);
 
-    text(`heading: ${this.velocity.heading()}`, this.position.x - 40, this.position.y - 50);
+    // text(`heading: ${this.velocity.heading()}`, this.position.x - 40, this.position.y - 50);
 
   }
 }
