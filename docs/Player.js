387a7a4571c610f26e3ee3a45fa12fb09e9ab09a
Lifguson <lwellsteadgi@gmail.com>
2025-02-25 12:40:52 +0000

    Integrated main with sprites branch and prettified code

--- docs/Player.js
+++ docs/Player.js
@@ -6,7 +6,18 @@
 // https://editor.p5js.org/codingtrain/sketches/zcTpMCpA
 
 class Player extends Sprite {
-  constructor(mainX, mainY, mainMass, velLimit, canal, boatFrames, timer, maxHealth, collisionDamage, damageOverTim) {
+  constructor(
+    mainX,
+    mainY,
+    mainMass,
+    velLimit,
+    canal,
+    boatFrames,
+    timer,
+    maxHealth,
+    collisionDamage,
+    damageOverTime
+  ) {
     //also includes the Includes the p5.collide2D addon library (https://github.com/bmoren/p5.collide2D?tab=readme-ov-file#collidepointellipse)
     // 'super' calls the parent constructor, passing it the appropriate sprite sheet
     super(mainX, mainY, boatFrames);
@@ -50,14 +61,18 @@ class Player extends Sprite {
 
     // If health is zero, stops player boat until repaired.
     if (this.zeroHealth) {
-      text("Health is zero! Repairs needed...", this.position.x, this.position.y + 20);
+      // text(
+      //   "Health is zero! Repairs needed...",
+      //   this.position.x,
+      //   this.position.y + 20
+      // );
       this.haltPlayer();
     }
 
     // If 'r' key is pressed, repair boat
     if (keyIsDown(82) === true) {
       // If health is zero, repairs take three times as long as default
-      if (this.zeroHealth) this.repair(9.0)
+      if (this.zeroHealth) this.repair(9.0);
       this.repair();
     }
   }
@@ -70,59 +85,61 @@ class Player extends Sprite {
     this.reachedTheNextOne(this.canal);
 
     let hitUp = this.didHitBorder(0); // 0 - upper border
-    let hitDown = this.didHitBorder(1); // 1 - lower border 
+    let hitDown = this.didHitBorder(1); // 1 - lower border
     let hitLeft = this.didHitBorder(2); // 2 - left border
     let hitRight = this.didHitBorder(3); // 3 - right border
-    this.hitAny = (hitUp || hitDown || hitRight || hitLeft);
+    this.hitAny = hitUp || hitDown || hitRight || hitLeft;
 
-    
-    if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) { // W = 83
+    if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
+      // W = 83
       this.applyForce(createVector(0, 0.5));
-    } else if (
-      keyIsDown(UP_ARROW) === true &&
-      this.position.y > setting.getUpperLimit(this.position.x)
-    ) {
+    } else if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
+      // S = 87
       this.applyForce(createVector(0, -0.5));
     }
-    else if (keyIsDown(UP_ARROW) || keyIsDown(87)) { // S = 87
-      this.applyForce(createVector(0,- 0.5));
-    }
-    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) { // A = 65
+    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
+      // A = 65
       this.applyForce(createVector(-0.5, 0));
-    }
-    else if (keyIsDown(RIGHT_ARROW) | keyIsDown(68)) { // D = 68
+    } else if (keyIsDown(RIGHT_ARROW) | keyIsDown(68)) {
+      // D = 68
       this.applyForce(createVector(0.5, 0));
     }
 
-    if (this.hitAny) { 
+    if (this.hitAny) {
       this.acceleration = createVector(0, 0);
       this.velocity.div(1000);
 
-      if (hitUp) {//collision mechanism for the upper border
+      if (hitUp) {
+        //collision mechanism for the upper border
         this.collisionOffset.set(0, 2.5);
       }
-      if (hitDown) { //collision mechanism for the bottom border
-         this.collisionOffset.set(0, -2.5);
+      if (hitDown) {
+        //collision mechanism for the bottom border
+        this.collisionOffset.set(0, -2.5);
       }
-      if (hitRight) { //collision mechanism for the right border
+      if (hitRight) {
+        //collision mechanism for the right border
         this.collisionOffset.set(-2.5, 0);
       }
-      if (hitLeft) { //collision mechanism for the left border
+      if (hitLeft) {
+        //collision mechanism for the left border
         this.collisionOffset.set(2.5, 0);
       }
     }
 
     this.applyCollisionOffset();
+
+    // Keep the x and y, inherited from Sprite class, in sync with the position
+    // this.x = this.position.x;
+    // this.y = this.position.y;
   }
 
-
   // this function basically draggs the player model off the border
-  // when the boat "hits" the edge of the canal 
+  // when the boat "hits" the edge of the canal
   applyCollisionOffset() {
-
     this.position.add(this.collisionOffset);
 
-    if (this.collisionOffset.x >  0.2) {
+    if (this.collisionOffset.x > 0.2) {
       this.collisionOffset.x -= 0.2;
     } else if (this.collisionOffset.x < -0.2) {
       this.collisionOffset.x += 0.2;
@@ -130,7 +147,7 @@ class Player extends Sprite {
       this.collisionOffset.x = 0;
     }
 
-    if (this.collisionOffset.y >  0.2) {
+    if (this.collisionOffset.y > 0.2) {
       this.collisionOffset.y -= 0.2;
     } else if (this.collisionOffset.y < -0.2) {
       this.collisionOffset.y += 0.2;
@@ -140,7 +157,7 @@ class Player extends Sprite {
   }
 
   didHitBorder(direction) {
-    let x = this.position.x; 
+    let x = this.position.x;
     let y = this.position.y;
     let angle = this.velocity.heading();
 
@@ -148,79 +165,80 @@ class Player extends Sprite {
     //you can create 3 circles with the following x&y coordinates.
     //One would be in the front of the ellipse, and 2 on each side.
     //The sin and cos are needed, because the origianl ellipse does not
-    //actually rotate, event though it is displayed as rotated along 
+    //actually rotate, event though it is displayed as rotated along
     // the velocity vector
     // Note, these are the real hitboxes. The coloured circles on the boat
     // (created in the paintPlayerModel()) are there for aesthetic reasons
     let frontHitbox = {
-      x: x + ((this.w/2) * cos(angle)),
-      y: y + ((this.w/2) * sin(angle)),
+      x: x + (this.height / 2) * cos(angle),
+      y: y + (this.height / 2) * sin(angle),
     };
     let rightHitbox = {
-      x: x + ((this.h/2) * -1 * sin(angle)),
-      y: y + ((this.h/2) * cos(angle)),
+      x: x + (this.width / 2) * -1 * sin(angle),
+      y: y + (this.width / 2) * cos(angle),
     };
     let leftHitbox = {
-      x: x + ((this.h/2) * sin(angle)),
-      y: y + ((this.h/2) * -1 * cos(angle)),
-    }
+      x: x + (this.width / 2) * sin(angle),
+      y: y + (this.width / 2) * -1 * cos(angle),
+    };
 
     let upLim = this.canal.getUpperLimit(x);
     let lowLim = this.canal.getLowerLimit(x);
     let leftLim = this.canal.getLeftLimit(y);
     let rightLim = this.canal.getRightLimit(y);
 
-    switch(direction) {
+    switch (direction) {
       case 0: //Does front, right or left hitbox cross the upper border?
-        let hitUpperFront = (frontHitbox.y <= upLim);
-        let hitUpperRight = (rightHitbox.y <= upLim);
-        let hitUpperLeft = (leftHitbox.y <= upLim);
-        return (hitUpperFront || hitUpperRight || hitUpperLeft);
+        let hitUpperFront = frontHitbox.y <= upLim;
+        let hitUpperRight = rightHitbox.y <= upLim;
+        let hitUpperLeft = leftHitbox.y <= upLim;
+        return hitUpperFront || hitUpperRight || hitUpperLeft;
       case 1: //Does front, right or left hitbox cross the lower border?
-        let hitLowerFront = (frontHitbox.y >= lowLim);
-        let hitLowerRight = (rightHitbox.y >= lowLim);
-        let hitLowerLeft = (leftHitbox.y >= lowLim);
-        return (hitLowerFront || hitLowerRight || hitLowerLeft);
+        let hitLowerFront = frontHitbox.y >= lowLim;
+        let hitLowerRight = rightHitbox.y >= lowLim;
+        let hitLowerLeft = leftHitbox.y >= lowLim;
+        return hitLowerFront || hitLowerRight || hitLowerLeft;
       case 2: //Does front, right or left hitbox cross the left border?
-        let hitLeftFront = (frontHitbox.x <= leftLim);
-        let hitLeftRight = (rightHitbox.x <= leftLim);
-        let hitLeftLeft = (leftHitbox.x <= leftLim);
-        return (hitLeftFront || hitLeftRight || hitLeftLeft);
+        let hitLeftFront = frontHitbox.x <= leftLim;
+        let hitLeftRight = rightHitbox.x <= leftLim;
+        let hitLeftLeft = leftHitbox.x <= leftLim;
+        return hitLeftFront || hitLeftRight || hitLeftLeft;
       case 3: //Does front, right or left hitbox cross the right border?
-        let hitRightFront = (frontHitbox.x >= rightLim);
-        let hitRightRight = (rightHitbox.x >= rightLim);
-        let hitRightLeft = (leftHitbox.x >= rightLim);
-        return (hitRightFront || hitRightRight || hitRightLeft);
+        let hitRightFront = frontHitbox.x >= rightLim;
+        let hitRightRight = rightHitbox.x >= rightLim;
+        let hitRightLeft = leftHitbox.x >= rightLim;
+        return hitRightFront || hitRightRight || hitRightLeft;
     }
-
-    // Keep the x and y, inherited from Sprite class, in sync with the position
-    this.x = this.position.x;
-    this.y = this.position.y;
   }
 
-  reachedTheNextOne(currCanal){ //Leah's function that checks the transition between canals (2 parallel lines)
-    let pasturesNew = currCanal.thresholdCheck(this.position.x, this.position.y);
-    if(pasturesNew != null){
-        this.canal = pasturesNew;
-        console.log("switched to canal with name " + this.canal.name)
+  reachedTheNextOne(currCanal) {
+    //Leah's function that checks the transition between canals (2 parallel lines)
+    let pasturesNew = currCanal.thresholdCheck(
+      this.position.x,
+      this.position.y
+    );
+    if (pasturesNew != null) {
+      this.canal = pasturesNew;
+      console.log("switched to canal with name " + this.canal.name);
     }
   }
 
   //the formula for friction is F = -v (reverced copy of the velocity vector) * Mu (arbitrary constant) * N (for our purpose can be equated to object's mass)
   // change the mu parameter if you want to increase/decrease friction
   friction() {
-    if (this.velocity.mag() > 0.02 && (!this.hitAny)){ //this is a bugfix (without it, the stationary object would flip 60 times a second)
+    if (this.velocity.mag() > 0.02 && !this.hitAny) {
+      //this is a bugfix (without it, the stationary object would flip 60 times a second)
       let friction = this.velocity.copy().normalize().mult(-1);
       // Magnitude of Friction
       friction.setMag(this.mu * this.mass);
       this.applyForce(friction);
-      // text(`friction: ${friction.mag()}`, this.position.x - 40, this.position.y - 35); 
-    } 
+      // text(`friction: ${friction.mag()}`, this.position.x - 40, this.position.y - 35);
+    }
   }
 
   updatePosition() {
     this.velocity.add(this.acceleration);
-    this.position.add(this.velocity); 
+    this.position.add(this.velocity);
     this.velocity.limit(this.velocityLimit);
     this.acceleration.set(0, 0);
   }
@@ -257,27 +275,59 @@ class Player extends Sprite {
     // //uncomment these 3 circles, if you need to adjust the position of the hitboxes
 
     // noStroke();
-    // fill('green');
-    // circle(this.position.x + ((this.w/2) * cos(this.velocity.heading())), this.position.y + ((this.w/2) * sin(this.velocity.heading())), 5);
-    // fill('blue');
-    // circle(this.position.x + ((this.h/2) * -1 * sin(this.velocity.heading())), this.position.y + ((this.h/2) * cos(this.velocity.heading())), 5);
-    // fill('red');
-    // circle(this.position.x + ((this.h/2) * sin(this.velocity.heading())), this.position.y + ((this.h/2) * -1 * cos(this.velocity.heading())), 5);
+    // fill("green");
+    // circle(
+    //   this.position.x + (this.height / 2) * cos(this.velocity.heading()),
+    //   this.position.y + (this.height / 2) * sin(this.velocity.heading()),
+    //   5
+    // );
+    // fill("blue");
+    // circle(
+    //   this.position.x + (this.width / 2) * -1 * sin(this.velocity.heading()),
+    //   this.position.y + (this.width / 2) * cos(this.velocity.heading()),
+    //   5
+    // );
+    // fill("red");
+    // circle(
+    //   this.position.x + (this.width / 2) * sin(this.velocity.heading()),
+    //   this.position.y + (this.width / 2) * -1 * cos(this.velocity.heading()),
+    //   5
+    // );
 
-    // // uncomment these lines if you want to see the borders of the canal at any given time
+    // // // uncomment these lines if you want to see the borders of the canal at any given time
 
     // fill(0);
-    // stroke('white');
+    // stroke("white");
     // strokeWeight(5);
 
     // // upper
-    // line(0, this.canal.getUpperLimit(this.position.x), windowWidth, this.canal.getUpperLimit(this.position.x));
+    // line(
+    //   0,
+    //   this.canal.getUpperLimit(this.position.x),
+    //   windowWidth,
+    //   this.canal.getUpperLimit(this.position.x)
+    // );
     // // lower
-    // line(0, this.canal.getLowerLimit(this.position.x), windowWidth, this.canal.getLowerLimit(this.position.x)); 
+    // line(
+    //   0,
+    //   this.canal.getLowerLimit(this.position.x),
+    //   windowWidth,
+    //   this.canal.getLowerLimit(this.position.x)
+    // );
     // // left
-    // line(this.canal.getLeftLimit(this.position.y), 0, this.canal.getLeftLimit(this.position.y), windowHeight);
+    // line(
+    //   this.canal.getLeftLimit(this.position.y),
+    //   0,
+    //   this.canal.getLeftLimit(this.position.y),
+    //   windowHeight
+    // );
     // // right
-    // line(this.canal.getRightLimit(this.position.y), 0, this.canal.getRightLimit(this.position.y), windowHeight);
+    // line(
+    //   this.canal.getRightLimit(this.position.y),
+    //   0,
+    //   this.canal.getRightLimit(this.position.y),
+    //   windowHeight
+    // );
   }
 
   // this function converts the force into acceleration (Force = Mass * Acceleration -> A = F/M),
@@ -288,34 +338,33 @@ class Player extends Sprite {
   }
 
   debugHelperText() {
-    fill("black");
-    stroke("white");
-
-    text(
-      `upper: ${Math.round(this.canal.getUpperLimit(this.position.x))}`,
-      this.position.x - 40,
-      this.position.y - 110
-    );
-    text(
-      `right: ${Math.round(this.canal.getRightLimit(this.position.y))}`,
-      this.position.x - 40,
-      this.position.y - 95
-    );
-    text(
-      `left: ${Math.round(this.canal.getLeftLimit(this.position.y))}`,
-      this.position.x - 40,
-      this.position.y - 80
-    );
-    text(
-      `lower: ${Math.round(this.canal.getLowerLimit(this.position.x))}`,
-      this.position.x - 40,
-      this.position.y - 65
-    );
-    text(
-      `x: ${Math.floor(this.position.x)} y: ${Math.floor(this.position.y)}`,
-      this.position.x - 40,
-      this.position.y - 50
-    );
+    // fill("black");
+    // stroke("white");
+    // text(
+    //   `upper: ${Math.round(this.canal.getUpperLimit(this.position.x))}`,
+    //   this.position.x - 40,
+    //   this.position.y - 110
+    // );
+    // text(
+    //   `right: ${Math.round(this.canal.getRightLimit(this.position.y))}`,
+    //   this.position.x - 40,
+    //   this.position.y - 95
+    // );
+    // text(
+    //   `left: ${Math.round(this.canal.getLeftLimit(this.position.y))}`,
+    //   this.position.x - 40,
+    //   this.position.y - 80
+    // );
+    // text(
+    //   `lower: ${Math.round(this.canal.getLowerLimit(this.position.x))}`,
+    //   this.position.x - 40,
+    //   this.position.y - 65
+    // );
+    // text(
+    //   `x: ${Math.floor(this.position.x)} y: ${Math.floor(this.position.y)}`,
+    //   this.position.x - 40,
+    //   this.position.y - 50
+    // );
   }
 
   // EVERYTHING BELOW THIS IS NEW -> DAMAGE MECHANICS!
@@ -344,7 +393,6 @@ class Player extends Sprite {
     }
   }
 
-
   // Decrements health by [damagePoints] points every [timeInterval] seconds.
   takeDamageOverTime(timeInterval = 2.0) {
     // Get current time from Main timer (started during setup)
@@ -364,7 +412,6 @@ class Player extends Sprite {
 
   // Returns health attribute to maxHealth
   repair(timeTaken = 3.0) {
-
     // Stop movement for 3 seconds
     this.haltPlayer(timeTaken);
 
@@ -372,7 +419,6 @@ class Player extends Sprite {
     this.health = this.maxHealth;
     this.zeroHealth = false;
   }
-
 
   // Stops boat for a given amount of time
   haltPlayer(timeHalted = null) {
@@ -384,9 +430,9 @@ class Player extends Sprite {
     if (timeHalted != null) {
       let timer = new Timer();
       timer.startTimer();
-    
+
       //text("Repairing...", this.position.x, this.position.y + 5);
-      // While timer < timeHalted, boat is immobile. 
+      // While timer < timeHalted, boat is immobile.
       while (timer.hasElapsed(timeHalted) === false) {
         // Print a text message to boat position saying that repairs are ongoing...
         //text("Repairing...", 500, 500);
