311bf85445950d9f749a35b87ef5f65272905bd2
Daniil Lutskiy <ya24711@bristol.ac.uk>
2025-04-06 14:57:35 +0100

    fixed a bug with max velocity cap doing nothing

--- Daniil_experimentation_withP5sprites/camera_experiment.js
+++ Daniil_experimentation_withP5sprites/camera_experiment.js
@@ -8,13 +8,15 @@ function setup() {
 	new Canvas(windowWidth, windowHeight);
 	// world.gravity.y = 5;
 
-  player = new Sprite(100, 100, 50, 25);
   centre = new Sprite(0, 0, 50);
   centre.colour = "red";
   centre.collider = 'static';
 
-  player.maxSpeed = 5;
-  // player.friction = 10;
+
+  player = new Sprite(100, 100, 50, 25);
+  // player.debug = true;
+  // player.maxSpeed = 5;
+  player.friction = 10;
   player.drag = 5;
   player.bounciness = 0.9;
   player.colour = 'green'; 
@@ -56,11 +58,7 @@ function draw() {
     }
   }
 
-  //debug info with coordinates ont pot of mivng player
-  text(`player.x: ${round(player.x)} player.y: ${round(player.y)}`, player.x, player.y - 30);
-  text(`p.canv.x: ${round()} p.canv.y: ${round(player.canvasPos.y)}`, player.x, player.y - 50);
-  text(`windowWidth/4: ${round(windowWidth/4)} windowWidth*3/4: ${round(windowWidth*3/4)}`, player.x, player.y - 70);
-  text(`windowHeight/4: ${round(windowHeight/4)} windowHeight*3/4: ${round(windowHeight*3/4)}`, player.x, player.y - 90);
+
 
 
   // player sprite movement logic
@@ -69,9 +67,20 @@ function draw() {
   if (kb.pressing('up')) player.applyForce(0, -40);
   else if (kb.pressing('down')) player.applyForce(0, 40);
 
-  let v = createVector(player.vel.x, player.vel.y);
-  let heading = v.heading();
-  player.rotation = heading;
+  // let v = createVector(player.vel.x, player.vel.y);
+  // let heading = v.heading();
+  // player.rotation = heading;
+
+  let maxSpeed = 5;
+  let currentVel = createVector(player.vel.x, player.vel.y);
+  if (currentVel.mag() > maxSpeed) {
+    currentVel.setMag(maxSpeed);
+    player.vel.x = currentVel.x;
+    player.vel.y = currentVel.y;
+  }
+
+  // Set sprite rotation based on velocity heading
+  player.rotation = currentVel.heading();
 
   if (kb.pressing('shift') && mouse.presses()){
     // console.log("Shift if pressed when mouse is clicked");
@@ -93,6 +102,13 @@ function draw() {
     // console.log(mouse.x);
 
   }
+
+    //debug info with coordinates ont pot of mivng player
+    text(`player.x: ${round(player.x)} player.y: ${round(player.y)}`, player.x, player.y - 30);
+    text(`player vel: ${round(currentVel.mag())}`, player.x, player.y - 50);
+    // text(`p.canv.x: ${round(player.canvasPos.x)} p.canv.y: ${round(player.canvasPos.y)}`, player.x, player.y - 50);
+    // text(`windowWidth/4: ${round(windowWidth/4)} windowWidth*3/4: ${round(windowWidth*3/4)}`, player.x, player.y - 70);
+    // text(`windowHeight/4: ${round(windowHeight/4)} windowHeight*3/4: ${round(windowHeight*3/4)}`, player.x, player.y - 90); 
 
   // // like with camera on, if I touch it, everything breaks
   // camera.off();
