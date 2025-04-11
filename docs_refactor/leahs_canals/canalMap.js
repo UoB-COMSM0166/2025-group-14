586f2cffa114ea1522f2ae2b905334f28811294f
LeahFrancesca <leahfrancesca64@gmail.com>
2025-04-11 15:44:37 +0100

    minor change to map code

--- docs_refactor/leahs_canals/canalMap.js
+++ docs_refactor/leahs_canals/canalMap.js
@@ -7,7 +7,6 @@ class canalMap {
         let c1 = new canal(300, 2, 100, this.player, garbageOn); //right, up
         let c2 = new canal(770, 4.5, 150, this.player, garbageOn); //right, down
         let c3 = new lock(470, 7, 130, this.player); //left, down
-<<<<<<< HEAD
         let c4 = new canal(600, 10, 220, this.player); //left up
         let c5 = new canal(400, 9, 60, this.player);
         this.networks.push(new canalNetwork(50, -350, [c1, c2, c3, c4 , c5]))
@@ -30,13 +29,6 @@ class canalMap {
         }
 
         console.log("outer: " + this.bankSprites.length);
-=======
-        let c4 = new canal(600, 10, 220, this.player, garbageOn); //left up
-        let c5 = new canal(400, 9, 60, this.player, garbageOn);
-        this.networks.push(new canalNetwork(-50, -350, [c1, c2, c3, c4, c5]))
-        print("Canal network x and y:" + this.networks[0].x + ", " + this.networks[0].y);
-
->>>>>>> main
     }
 
     forAllNetworks(callback){
