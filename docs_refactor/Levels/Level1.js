b28d9a6edbeaae6fb660b6f5c22ab89e0d60699b
Daniil Lutskiy <ya24711@bristol.ac.uk>
2025-04-14 14:10:53 +0100

    added updated the level1.js file

--- docs_refactor/Levels/Level1.js
+++ docs_refactor/Levels/Level1.js
@@ -1,50 +1,65 @@
 class Level1 {
     
   constructor() {
-      // Post-refactor globals
-      this.banks;
-      this.leftBankConstr; 
-      this.rightBankConstr;
-      this.player;
-      this.playerCfg;
-      this.pursuer;
-      this.pursuerCfg;
-      this.centreCircle;
-      //canals
-      this.testEdge;
-      this.leftBankConstr;
-      this.rightBankConstr;
-      this.map;
-      /* this.playerAnimation = loadAnimation("Boat-redbrown.png", [
-          [64, 64, 64, 32],
-          [0, 0, 64, 32],
-          [0, 64, 64, 32],
-      ]);
-      this.pursuerAnimation = loadAnimation("Boat-grey.png", [
+    // Post-refactor globals
+    this.banks;
+    this.leftBankConstr; 
+    this.rightBankConstr;
+    this.player;
+    this.playerCfg;
+    this.pursuer;
+    this.pursuerCfg;
+    this.centreCircle;
+    //canals
+    this.testEdge;
+    this.leftBankConstr;
+    this.rightBankConstr;
+    this.map;
+    /* this.playerAnimation = loadAnimation("Boat-redbrown.png", [
         [64, 64, 64, 32],
         [0, 0, 64, 32],
         [0, 64, 64, 32],
-      ]); */
+    ]);
+    this.pursuerAnimation = loadAnimation("Boat-grey.png", [
+      [64, 64, 64, 32],
+      [0, 0, 64, 32],
+      [0, 64, 64, 32],
+    ]); */
 
-      // below is to make sure that animations are only loaded in once
-      this.playerAnimation = LevelController.playerAnimation;
-      this.pursuerAnimation = LevelController.pursuerAnimation;
+    // below is to make sure that animations are only loaded in once
+    this.playerAnimation = LevelController.playerAnimation;
+    this.pursuerAnimation = LevelController.pursuerAnimation;
 
-      this.timer;
-      this.healthbar;
+    this.timer;
+    this.healthbar;
+
+    if (difficultyLevel === 0) { //easy level
       this.playerMaxHealth = 100;
       this.canalCollisionDamage = 3;
       this.damageOverTime = 1;
-  }
+      this.playerSpeed = 4.5;
+      this.pursuerSpeed = 3;
+    } else if (difficultyLevel === 1) { //medium level
+      this.playerMaxHealth = 70;
+      this.canalCollisionDamage = 5;
+      this.damageOverTime = 1.2;
+      this.playerSpeed = 4.5
+      this.pursuerSpeed = 3;
+    } else if (difficultyLevel === 2) { //hard level
+      this.playerMaxHealth = 50;
+      this.canalCollisionDamage = 10;
+      this.damageOverTime = 1.5;
+      this.playerSpeed = 4.5
+      this.pursuerSpeed = 3;
+    }
+}
 
   setup() {
        // Instantiate Timer (to time events that occur over time)
       this.timer = new Timer();
       this.timer.startTimer();
-
-      this.centreCircle = new CentreCirlce();
-
-      this.player = new Sprite(100, 100, 50, 25, 4.5);
+      
+      this.player = new Sprite(100, 100, 50, 25, this.playerSpeed);
 
       this.map = new canalMap(this.player);
 
@@ -56,7 +71,7 @@ class Level1 {
       this.pursuer = new Sprite(40, 100, 50, 25);
       this.pursuer.addAnimation("boat", this.pursuerAnimation);
       this.pursuer.animation.frameDelay = 18;
-      this.pursuerCfg = new PursuerConfig(this.pursuer, this.player, 3);
+      this.pursuerCfg = new PursuerConfig(this.pursuer, this.player, this.pursuerSpeed);
     
       // Instantiate healthbar
       this.healthbar = new HealthBar(this.playerMaxHealth, this.playerCfg);
@@ -87,15 +102,16 @@ class Level1 {
       this.coordinateGrid();
 
       if (this.playerCfg.isHealthZero()){
-          this.clearSprites();
-          state = GameState.LOSE;
+        this.clearSprites();
+        state = GameState.LOSE;
       }
-      if (keyCode == 87){
+      if (kb.pressed('q') || finishLineCrossed){ // changes w keycode to q
           this.clearSprites();
           state = GameState.WIN;
+          finishLineCrossed = false;
       }
       
-      if (keyCode == 27) {
+      if (kb.pressed('escape')) {
           this.clearSprites();
           state = GameState.START_SCREEN;
       }
@@ -104,14 +120,7 @@ class Level1 {
   clearSprites() {
     this.player.remove();
     this.pursuer.remove();
-    this.centreCircle.remove();
     this.map.removeSprites();
-    /*this.c1.removeSprites();
-    this.c2.removeSprites();
-    this.c3.removeSprites();
-    this.c4.removeSprites();
-    this.c5.removeSprites();*/
-      
   }
 
   coordinateGrid() {
